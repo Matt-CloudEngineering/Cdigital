1b68f4a61dd0394c2cb3daba8e69a98761a585b7
Matt Smith <mattsmith@Matts-MacBook-Pro.local>
2023-07-17 15:04:26 -0400

    Assets

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Your Website Title</title>
-  <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+  <link rel="stylesheet" href="{{ asset('styles.css') }}">
 </head>
 
 <body>
@@ -58,7 +58,7 @@
     </div>
   </footer>
 
-  <script src="{{ asset('js/slider.js') }}"></script>
+  <script src="{{ asset('slider.js') }}"></script>
 </body>
 
 </html>
